60252322dad3e91022e21495a20ac4ae88dfb835
Vitor França <68481246+V-kr0pt@users.noreply.github.com>
2020-12-13 14:08:27 -0300

    Add do Circuito resolvido no Tempo em matlab

---
+++ Circuito_no_Tempo/Circuito_no_Tempo.m
@@ -0,0 +1,106 @@
+%Trabalho de circuitos Laplace
+clear
+
+%declarando s como variável simbólica
+syms s;
+
+%Receber arquivo csv e transformar a matriz de entrada para o formato sym
+dados = csvread('tabela_de_entrada.csv',1,0);
+
+% matriz dos nós
+nos= double(dados(:,2:3))
+
+%o número de nós será dado pelo máximo valor de nó de 
+%ambas as colunas
+numero_de_nos = (max(max(nos))
+
+%o número de ramos será dado pelo número de linhas 
+%do arquivo
+tamanho_do_arq = size(dados)
+numero_de_ramos = tamanho_do_arq(1) 
+
+%matriz de incidência completa (n x b):
+%incializando
+matriz_inc_completa = zeros(numero_de_nos, numero_de_ramos)
+
+%atribuindo orientação dos grafos
+for ramo = 1:numero_de_ramos
+    matriz_inc_completa(nos(ramo,1), ramo) = 1
+    matriz_inc_completa(nos(ramo,2), ramo) = -1
+end
+
+%matriz reduzida
+
+% como matriz_inc_completa é LD, podemos trabalhar com
+% com uma matriz reduzida (A), retirando a última linha
+A = matriz_inc_completa(1:end-1,:)
+%A transposta da matriz reduzida:
+A_T = transpose(A) 
+
+% Matriz de admitância Yb:
+
+%impedância resultante para cada ramo:
+
+%resistiva
+R = double(dados(:, 4))
+%indutiva
+L = double(dados(:,5))
+Xl = s * L
+%capacitiva
+Xc = 1/(s*double(dados(:,7)))
+Xc = transpose(Xc)
+Xc(Xc(:) == Inf)=0
+
+%impedância equivalente 
+Zeq = R + Xl + Xc
+
+%admitância equivalente
+Yeq = 1./Zeq(:)
+
+%Criando a matriz admitância de ramo: Yb (diagonal)
+Yb = diag(Yeq)
+
+%Admitância de nó
+Yn = A * Yb * A_T
+
+% -- Fontes de Independentes --
+
+%Fontes de tensão
+%coluna 9: Vind, coluna 8: V0
+Vs = double(dados(:,9)) + double(dados(:,8))
+%coluna 6: I0
+Vs = Vs/s + L.*double(dados(:,6))
+
+%Fonte de corrente
+Js = double(dados(:,10))
+
+%Cálculo do Is:
+Is = A * Yb * Vs - A* Js
+
+%Cálculo do Tensão de nó
+E = inv(Yn) * Is
+
+%Cálculo da Tensão de ramo
+V = A_T * E
+
+%Corrente nos Ramo:
+J = Js + Yb*V - Yb*Vs
+
+%Transformada Inversa de Laplace
+
+%corrente nos ramo no tempo:
+j = ilaplace(J)
+
+%tensão nos ramos no tempo:
+v = ilaplace(V)
+
+%tensão nos nós no tempo:
+e = ilaplace(E)
+
+%gráficos
+t = 0:0.01:10
+e_t = subs(e,t)
+plot(t,e_t)
+
+
+
